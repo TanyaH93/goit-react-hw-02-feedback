3e86552453b877f06d796120da1d2d5091a9d44d
Tanya <fullstackon@gmail.com>
2020-05-23 19:39:44 +0300

    tas2-1

---
+++ build/precache-manifest.19ca0447b8ccf6bb603c640d8f82cd14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9a5f624a37c0385fbf460c83bcbed7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ee6d5c4e7e90d96c740",
+    "url": "/static/css/main.ab8f1d27.chunk.css"
+  },
+  {
+    "revision": "ad24971988a102cbb8ce",
+    "url": "/static/js/2.4a954e0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a954e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee6d5c4e7e90d96c740",
+    "url": "/static/js/main.977d9ffd.chunk.js"
+  },
+  {
+    "revision": "793231814425875c9813",
+    "url": "/static/js/runtime-main.d98b48ce.js"
+  }
+]);
