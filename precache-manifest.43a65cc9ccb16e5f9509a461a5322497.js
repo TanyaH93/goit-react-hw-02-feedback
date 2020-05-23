a068a7ab1d873dfc9359683eadcf50e51e1cb337
Tanya <fullstackon@gmail.com>
2020-05-23 19:50:17 +0300

    Updates

---
+++ precache-manifest.43a65cc9ccb16e5f9509a461a5322497.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7322ac5e621ccfd9c789c6a37fbc677e",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "6ee6d5c4e7e90d96c740",
+    "url": "/goit-react-hw-02-feedback/static/css/main.ab8f1d27.chunk.css"
+  },
+  {
+    "revision": "ad24971988a102cbb8ce",
+    "url": "/goit-react-hw-02-feedback/static/js/2.4a954e0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.4a954e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee6d5c4e7e90d96c740",
+    "url": "/goit-react-hw-02-feedback/static/js/main.977d9ffd.chunk.js"
+  },
+  {
+    "revision": "67fc603d4fe28264fbd4",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.56a9b6a1.js"
+  }
+]);
